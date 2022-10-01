f90a38298d91c399e446555775700761ef6eaad2
PRATIMA <107994434+PRATIMASINGH123@users.noreply.github.com>
2022-10-01 13:28:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,5 +3,9 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use((req, res, next) => {
+  next({ status:200, message : 'Hello {name}, you are {age} years old:${req.origionalUrl}'});
+});
 
+        
 module.exports = app;
